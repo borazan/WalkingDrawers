03dd481e94173e8e265a4766619b7db27d013def
borazan <boratuncel@gmail.com>
2024-02-12 00:00:58 +0100

    implement bias

--- core/src/com/mygdx/game/Drawer.java
+++ core/src/com/mygdx/game/Drawer.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Drawer {
     private int x;
@@ -10,6 +11,14 @@ public class Drawer {
     private int xMax;
     private int yMax;
     private double alpha;
+    private float biasXThreshold; //compared against Math.Random 0.5 in move()
+    private float biasYThreshold;
+    private final float biasChance = 0.0005f; //chance to enter a movement bias phase for the given duration
+    private final int biasDuration = 5; //in seconds, gets multiplied by 1000 for ms
+    private final float biasXIntensity = 0.2f; //how intense should this Drawer's biases be
+    private final float biasYIntensity = 0.2f;
+    private long biasEndTime = 0;
+    private boolean isBiased = false;
 
     public Drawer() {
         this.xMax = Gdx.graphics.getWidth();
@@ -19,6 +28,8 @@ public class Drawer {
         this.alpha = 1.0;
         this.color = new Color();
         color.set((float) Math.random(), (float) Math.random(), (float) Math.random(), (float) alpha);
+        this.biasXThreshold = 0.5f + (((float)Math.random() - 0.5f) * biasXIntensity);
+        this.biasYThreshold = 0.5f + (((float)Math.random() - 0.5f) * biasYIntensity);
     }
 
     public void update() {
@@ -27,9 +38,26 @@ public class Drawer {
     }
 
     public void move() {
-        x += (Math.random() > 0.5) ? 1 : -1;
-        y += (Math.random() > 0.5) ? 1 : -1;
+        long currentTime = TimeUtils.millis();
+        if (!isBiased) {
+            if (Math.random() <= biasChance) { //roll for bias, only if currently unbiased
+                isBiased = true;
+                biasEndTime = currentTime + (biasDuration * 1000);
+            }
+        } else { //currently biased
+            if (currentTime >= biasEndTime) {
+                isBiased = false;
+            }
+        }
 
+        if (isBiased) {
+            x += (Math.random() > biasXThreshold) ? 1 : -1;
+            y += (Math.random() > biasXThreshold) ? 1 : -1;
+
+        } else {
+            x += (Math.random() > 0.5) ? 1 : -1;
+            y += (Math.random() > 0.5) ? 1 : -1;
+        }
         x = clamp(x, 0, xMax - 1); //-1 since coordinates are 0-based
         y = clamp(y, 0, yMax - 1);
     }
